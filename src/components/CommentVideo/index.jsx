30d2c0c9f50f806ed382663ff896eb682f7b66a8
PhucKhanh <nthtpk7h7i@gmail.com>
2024-09-06 22:46:53 +0700

    update logic ui comment video, comment comment, ...

--- src/components/CommentVideo/index.jsx
+++ src/components/CommentVideo/index.jsx
@@ -7,14 +7,31 @@ import {
   ListItem,
   ListItemButton,
   Paper,
+  TextField,
   Typography,
 } from "@mui/material";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { useTheme } from "@emotion/react";
 import ConfirmDeleteCommentVideo from "../dialog/ConfirmDeleteCommentVideo";
+import InputCommentComment from "../InputCommentComment";
+import { ThemeContext } from "../../context/ThemeContext";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
+import EmojiPicker from "emoji-picker-react";
+
+const textFieldStyles = {
+  "& .MuiInput-underline:before": {
+    borderBottomColor: "customBorderBottomColorTextFieldStandard.main",
+  },
+  "& .MuiInput-underline:hover:not(.Mui-disabled):before": {
+    borderBottomColor: "customBorderBottomColorTextFieldStandard.main",
+  },
+  "& .MuiInput-underline:after": {
+    borderBottomColor: "text.primary",
+  },
+};
 
 export default function CommentVideo({
   avatar,
@@ -23,15 +40,47 @@ export default function CommentVideo({
   comment,
   type,
 }) {
-  const [showActionEditCommented, setShowActionEditCommented] = useState(false);
-  const editCommentedButtonRef = useRef(null);
-  const listEditCommentedRef = useRef(null);
-  const [openDialogConfirmDeleteComment, setOpenDialogConfirmDeleteComment] =
-    useState(false);
-
   const theme = useTheme();
 
+  const [showActionEditCommented, setShowActionEditCommented] = useState(false);
+  const [openDialogConfirmDeleteComment, setOpenDialogConfirmDeleteComment] =
+    useState(false);
+  const [openInputCommentComment, setOpenInputCommentComment] = useState(false);
+
+  const editCommentedButtonRef = useRef(null);
+  const listEditCommentedRef = useRef(null);
+
+  const { themeMode } = useContext(ThemeContext);
+
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+  const [valueEditComment, setValueEditComment] = useState(comment);
+  const [openInputComment, setOpenInputComment] = useState(false);
+
+  const toggleEmojiPicker = () => {
+    setShowEmojiPicker((prev) => !prev);
+  };
+
+  const handleCancelComment = () => {
+    setValueEditComment(comment);
+    setShowEmojiPicker(false);
+    setOpenInputComment(false);
+  };
+
+  const handlePostComment = () => {
+    console.log("Bình luận đã gửi:", valueEditComment);
+    handleCancelComment();
+  };
+
+  const handleComment = (e) => {
+    setValueEditComment(e.target.value);
+  };
+
+  const handleEmojiClick = (e) => {
+    setValueEditComment((prev) => prev + e.emoji);
+  };
+
   const toggleEditCommented = () => {
+    if (!showActionEditCommented) setOpenInputComment(false);
     setShowActionEditCommented((prev) => !prev);
   };
 
@@ -96,13 +145,91 @@ export default function CommentVideo({
               {dateTimeComment}
             </Typography>
           </Box>
-          <Typography variant='subtitle2'>{comment}</Typography>
-          <Typography
-            sx={{ fontSize: "13px", mt: "8px", cursor: "pointer" }}
-            fontWeight={600}
-          >
-            Phản hồi
-          </Typography>
+          {openInputComment ? (
+            <Box
+              sx={{ width: "100%", display: "flex", flexDirection: "column" }}
+            >
+              <TextField
+                variant='standard'
+                placeholder='Viết bình luận...'
+                sx={{ mb: "12px", ...textFieldStyles }}
+                value={valueEditComment}
+                onChange={handleComment}
+              />
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  width: "100%",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Box sx={{ position: "relative", ml: "12px" }}>
+                  <InsertEmoticonIcon
+                    sx={{ cursor: "pointer" }}
+                    onClick={toggleEmojiPicker}
+                  />
+                  {showEmojiPicker && (
+                    <EmojiPicker
+                      theme={themeMode}
+                      emojiStyle='native'
+                      lazyLoadEmojis={true}
+                      onEmojiClick={handleEmojiClick}
+                      style={{ position: "absolute", zIndex: "1000" }}
+                    />
+                  )}
+                </Box>
+                <Box>
+                  <Chip
+                    label='Hủy'
+                    sx={{
+                      p: "4px",
+                      fontSize: "14px",
+                      fontWeight: "600",
+                      userSelect: "none",
+                      bgcolor: "primary.main",
+                      cursor: "pointer",
+                      mr: "8px",
+                    }}
+                    onClick={handleCancelComment}
+                  />
+                  <Chip
+                    label='Bình luận'
+                    sx={{
+                      p: "4px",
+                      fontSize: "14px",
+                      fontWeight: "600",
+                      userSelect: "none",
+                      cursor: "pointer",
+                      bgcolor: valueEditComment ? "#3da2f9" : "",
+                      color: valueEditComment ? "secondary.main" : "",
+                      "&:hover": {
+                        bgcolor: "#3da2f9",
+                        opacity: "0.9",
+                      },
+                    }}
+                    disabled={valueEditComment === ""}
+                    onClick={handlePostComment}
+                  />
+                </Box>
+              </Box>
+            </Box>
+          ) : (
+            <Typography variant='subtitle2'>{comment}</Typography>
+          )}
+          {openInputCommentComment ? (
+            <InputCommentComment
+              setOpenInputCommentComment={setOpenInputCommentComment}
+            />
+          ) : (
+            <Typography
+              sx={{ fontSize: "13px", mt: "8px", cursor: "pointer" }}
+              fontWeight={600}
+              onClick={() => setOpenInputCommentComment(true)}
+            >
+              Phản hồi
+            </Typography>
+          )}
         </Box>
         <Box sx={{ position: "relative" }}>
           <IconButton
@@ -124,7 +251,13 @@ export default function CommentVideo({
               }}
             >
               <List>
-                <ListItem disablePadding>
+                <ListItem
+                  disablePadding
+                  onClick={() => {
+                    setOpenInputComment(true);
+                    setShowActionEditCommented(false);
+                  }}
+                >
                   <ListItemButton>
                     <EditIcon />
                     <Typography sx={{ ml: "12px" }}>Chỉnh sửa</Typography>
---
+++ src/components/InputCommentComment/index.jsx
@@ -0,0 +1,119 @@
+import { Avatar, Box, Chip, TextField } from "@mui/material";
+import React, { useContext, useState } from "react";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
+import EmojiPicker from "emoji-picker-react";
+import { ThemeContext } from "../../context/ThemeContext";
+
+const textFieldStyles = {
+  "& .MuiInput-underline:before": {
+    borderBottomColor: "customBorderBottomColorTextFieldStandard.main",
+  },
+  "& .MuiInput-underline:hover:not(.Mui-disabled):before": {
+    borderBottomColor: "customBorderBottomColorTextFieldStandard.main",
+  },
+  "& .MuiInput-underline:after": {
+    borderBottomColor: "text.primary",
+  },
+};
+
+export default function InputCommentComment({ setOpenInputCommentComment }) {
+  const { themeMode } = useContext(ThemeContext);
+
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+  const [valueComment, setValueComment] = useState("");
+
+  const toggleEmojiPicker = () => {
+    setShowEmojiPicker((prev) => !prev);
+  };
+
+  const handleCancelComment = () => {
+    setValueComment("");
+    setShowEmojiPicker(false);
+    setOpenInputCommentComment(false);
+  };
+
+  const handlePostComment = () => {
+    console.log("Bình luận đã gửi:", valueComment);
+    handleCancelComment();
+  };
+
+  const handleComment = (e) => {
+    setValueComment(e.target.value);
+  };
+
+  const handleEmojiClick = (e) => {
+    setValueComment((prev) => prev + e.emoji);
+  };
+
+  return (
+    <Box sx={{ display: "flex", mt: "24px", width: "100%" }}>
+      <Avatar alt='' src='' sx={{ width: "24px", height: "24px" }} />
+      <Box sx={{ width: "100%", display: "flex", flexDirection: "column" }}>
+        <TextField
+          variant='standard'
+          placeholder='Viết bình luận...'
+          sx={{ ml: "12px", mb: "12px", ...textFieldStyles }}
+          value={valueComment}
+          onChange={handleComment}
+        />
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            width: "100%",
+            justifyContent: "space-between",
+          }}
+        >
+          <Box sx={{ position: "relative", ml: "12px" }}>
+            <InsertEmoticonIcon
+              sx={{ cursor: "pointer" }}
+              onClick={toggleEmojiPicker}
+            />
+            {showEmojiPicker && (
+              <EmojiPicker
+                theme={themeMode}
+                emojiStyle='native'
+                lazyLoadEmojis={true}
+                onEmojiClick={handleEmojiClick}
+                style={{ position: "absolute", zIndex: "1000" }}
+              />
+            )}
+          </Box>
+          <Box>
+            <Chip
+              label='Hủy'
+              sx={{
+                p: "4px",
+                fontSize: "14px",
+                fontWeight: "600",
+                userSelect: "none",
+                bgcolor: "primary.main",
+                cursor: "pointer",
+                mr: "8px",
+              }}
+              onClick={handleCancelComment}
+            />
+            <Chip
+              label='Bình luận'
+              sx={{
+                p: "4px",
+                fontSize: "14px",
+                fontWeight: "600",
+                userSelect: "none",
+                cursor: "pointer",
+                bgcolor: valueComment ? "#3da2f9" : "",
+                color: valueComment ? "secondary.main" : "",
+                "&:hover": {
+                  bgcolor: "#3da2f9",
+                  opacity: "0.9",
+                },
+              }}
+              disabled={valueComment === ""}
+              onClick={handlePostComment}
+            />
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
